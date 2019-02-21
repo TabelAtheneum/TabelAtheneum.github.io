c402912fb288b7d9782eb0705364d830b3e9dfd7
TabelAtheneum <47224733+TabelAtheneum@users.noreply.github.com>
2019-02-21 14:55:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-function openvak(vak){
-	if($(vak).is(':visible') == false){
-		const y = findPos(document.getElementById(vak)) - 10;
-		window.scroll({top: y, behavior: 'smooth'});
-	}
-	var i;
-	if(vak == "#economie"){i = 0;}else if(vak == "#grieksLatijn"){i = 1;}else if(vak == "#humaneWetenschappen"){i = 2;}else if(vak == "#latijn"){i = 3;}else if(vak == "#wetenschappen"){i = 4;}
-	if($(vak).is(':visible') == false){
-		document.getElementsByTagName("i")[i].style.transform = "rotate(225deg)";
-		document.getElementsByTagName("i")[i].style.marginBottom = "-2px";
-	}
-	else {
-		document.getElementsByTagName("i")[i].style.transform = "rotate(45deg)";
-		document.getElementsByTagName("i")[i].style.marginBottom = "2px";
-	}
-	$(vak).toggle(250);
+function jaar1() {
+	window.location = "1ste jaar/index.html";
 }
 
-function findPos(obj) {
-    var curtop = 0;
-    if (obj.offsetParent) {
-        do {
-            curtop += obj.offsetTop;
-        } while (obj = obj.offsetParent);
-    return [curtop];
-    }
+function jaar2() {
+	window.location = "2de jaar/index.html";
+}
+
+function graad2() {
+	window.location = "2de graad/index.html";
+}
+
+function graad3() {
+	window.location = "3de graad/index.html";
 }
